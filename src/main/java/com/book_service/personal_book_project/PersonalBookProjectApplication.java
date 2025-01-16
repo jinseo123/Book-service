148e17afc3fd581c6ab23fcfdda9d0c7b435ee4e
jinseo123 <kjk0385@naver.com>
2025-01-16 21:42:04 +0900

    create project

---
+++ src/main/java/com/book_service/personal_book_project/PersonalBookProjectApplication.java
@@ -0,0 +1,13 @@
+package com.book_service.personal_book_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalBookProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalBookProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/book_service/personal_book_project/PersonalBookProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.book_service.personal_book_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalBookProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
