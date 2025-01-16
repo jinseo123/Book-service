51f5f9dc3d7f5a688d99ae71889bdfc2ee8e4ea9
jinseo123 <kjk0385@naver.com>
2025-01-16 23:02:11 +0900

    1

---
+++ src/main/java/com/book_service/personal_book_project/controller/MainController.java
@@ -0,0 +1,26 @@
+package com.book_service.personal_book_project.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequiredArgsConstructor
+public class MainController {
+    @GetMapping("/")
+    public String main(){
+        return "main";
+    }
+
+    @PostMapping("/auth/login")
+    public String login(){
+        return "xxx";
+    }
+
+    @GetMapping("/auth/login")
+    public String login_get(){
+        return "login";
+    }
+}
---
+++ src/main/java/com/book_service/personal_book_project/domain/user/User.java
@@ -0,0 +1,12 @@
+package com.book_service.personal_book_project.domain.user;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class User {
+}
